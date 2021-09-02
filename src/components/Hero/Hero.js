017abc99a85ce9d002dfca7f65310bba937a8f3c
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-09-02 11:35:44 +0530

    Project Section Completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row notepading>
+    <LeftSection>
+      <SectionTitle main center>
+    Welcome to <br />
+    My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        A self taught fullstack developer who fell in love with coding being a management student
+      </SectionText>
+      <Button onClick={() => window.location = "/about" }>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
