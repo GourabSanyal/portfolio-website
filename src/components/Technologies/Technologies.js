678a94843faadc81dac2dccb1908edeec289d50c
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-09-03 10:38:44 +0530

    Images location changed, technologies updated

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiPython, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <Section id='tech'>
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>I have worked in different Technologies in web developement.
       From Backend to design
     </SectionText>
     <List>
+      {/* front-end */}
+      <ListItem>
+        <DiReact size='3rem'/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js <br/>
+            Next.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+       {/* back-end */}
       <ListItem>
         <DiFirebase size='3rem'/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js <br/>
+            Firebase
+            
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+       {/* front-end */}
+      <ListItem>
+        <DiPython size='3rem'/>
+        <ListContainer>
+          <ListTitle>Languages</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            JavaScript <br/>
+            Python
+          </ListParagraph>
+        </ListContainer>
       </ListItem>
     </List>
   </Section>
