681879da6586b8cd53b33b1b1471a78c8ab58459
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-04-18 19:24:24 +0530

    hero updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
-
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <Section row notepading>
     <LeftSection>
       <SectionTitle main center>
-    Hey,<br />
-    I'm Gourab<br /> 
-    A self taught web developer
+        Hey,
+        <br />
+        I'm Gourab
+        <br />A web developer
       </SectionTitle>
-      <SectionText>        
-          Curious, Enthusiastic and I love to code :)                
-      </SectionText>
-      <Button onClick={() => window.location = "#about" }>Learn More</Button>
+      <SectionText>Curious, Enthusiastic and I love to code :)</SectionText>
+      <Button onClick={() => (window.location = "#about")}>Learn More</Button>
     </LeftSection>
   </Section>
 );
